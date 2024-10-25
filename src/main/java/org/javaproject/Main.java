cb1d7373e993990e9c4767c8400d72b361943383
VelazqueZzz <93205261+VelazqueZzz@users.noreply.github.com>
2024-10-25 22:51:14 +0300

    Add files via upload

--- src/main/java/org/javaproject/Main.java
+++ src/main/java/org/javaproject/Main.java
@@ -1,8 +1,34 @@
 package org.javaproject;
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-    Member member = new Member('S',1,"yvonne",100);
-    System.out.println(member);
+        String mem;
+        MembershipManagement mm=new MembershipManagement();
+  FileHandler fh = new FileHandler();
+        LinkedList<Member>members = fh.readFile();
+        int choise = mm.getChoice();
+        while(choise != -1){
+            switch(choise){
+                case 1:
+                    mem=mm.addMembers(members);
+                    fh.appendFile(mem);
+                    break;
+                    case 2:
+                        mm.removeMember(members);
+                        fh.overwriteFile(members);
+                        break;
+                        case 3:
+                            mm.printMemberInfo(members);
+                            break;
+                default:
+                    System.out.println("\nYou have selected an invalid option.\n\n");
+                    break;
+            }
+            choise = mm.getChoice();
+        }
+        System.out.println("\nGood bye!1");
+
     }
 }
